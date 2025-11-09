25dc3d456bed37c79d88f739e5a73c04db85e8d6
MinnnErrr <lohminer22@gmail.com>
2025-11-10 00:28:52 +0800

    edited navbar

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -8,6 +8,7 @@
     <title>Master Layout</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
     <style>
         .navbar {
             background-color: #0B131A !important;
@@ -16,15 +17,18 @@
         .navbar .nav-link,
         .navbar .navbar-brand {
             color: white !important;
-            margin-left: 25px;
-            margin-right: 25px;
-        }
-
-        .navbar .navbar-brand {
+            /* margin-left: 25px;
+            margin-right: 25px; */
             font-weight: 600;
         }
 
-        .navbar .nav-link.active,
+        .navbar .nav-link.active {
+            background-color: #81CBC0 !important;
+            color: #0B131A !important;
+            border-radius: 5px !important;
+            font-weight: 500;
+        }
+
         .navbar .nav-link:hover {
             background-color: #81CBC0 !important;
             color: #0B131A !important;
@@ -47,26 +51,36 @@
             color: #0B131A;
             font-weight: 500;
         }
-        
+
+        .navbar-toggler {
+            color: white;
+            font-size: 36px;
+        }
+
+        .navbar-toggler:focus,
+        .navbar-toggler:active {
+            outline: none;
+            box-shadow: none;
+        }
     </style>
 </head>
 
 <body>
     <nav class="navbar navbar-expand-lg">
         <div class="container-fluid">
-            <a class="navbar-brand" href="{{ route('home') }}">Personal Profiles</a>
+            <a class="navbar-brand ms-4" href="{{ route('home') }}">Personal Profiles</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                 aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+                <i class="bi bi-list"></i>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
+                <ul class="navbar-nav ms-auto mb-2 mt-2 mb-lg-0">
+                    <li class="nav-item me-4 ms-md-4 mb-md-2">
                         <a class="nav-link {{ request()->routeIs('home') ? 'active' : '' }}"
                             href="{{ route('home') }}">Home</a>
                     </li>
-                    <li class="nav-item dropdown">
+                    <li class="nav-item dropdown me-4 ms-md-4 mb-md-2">
                         <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
                             aria-expanded="false">
                             About Us
@@ -74,17 +88,17 @@
                         <ul class="dropdown-menu">
                             <li><a class="dropdown-item {{ request()->routeIs('CA22062') ? 'active' : '' }}"
                                     href="{{ route('CA22062') }}">Loh Min Er</a></li>
-                            
+
                             <li><a class="dropdown-item {{ request()->routeIs('CD22076') ? 'active' : '' }}"
                                     href="{{ route('CD22076') }}">Tai Xiao Ying</a></li>
 
                             <li><a class="dropdown-item" href="#">member 3</a></li>
-                            
+
                             <li><a class="dropdown-item {{ request()->routeIs('CB24017') ? 'active' : '' }}"
                                     href="{{ route('CB24017') }}">Tan Cheong Han</a></li>
                         </ul>
                     </li>
-                    <li class="nav-item">
+                    <li class="nav-item me-4 ms-md-4 mb-md-2">
                         <a class="nav-link {{ request()->routeIs('contact') ? 'active' : '' }}"
                             href="{{ route('contact') }}">Contact</a>
                     </li>
