a24709e009f3ca0957daac4825409b4f672114cb
MinnnErrr <lohminer22@gmail.com>
2025-11-11 00:23:14 +0800

    min's update

--- resources/views/about_CA22062.blade.php
+++ resources/views/about_CA22062.blade.php
@@ -4,7 +4,8 @@
 <div class="container-fluid profile-background p-5">
     <div class="row mb-5">
         <div class="col-12 col-lg-3 d-flex justify-content-center align-items-center mb-5">
-            <img src="{{ asset('images/miner.jpg') }}" style="width: 230px; height: 280px;" alt="Member 1">
+            <img src="{{ asset('images/miner.jpg') }}" class='img-fluid rounded-4'
+                style="max-width: 230px; max-height: 280px;" alt="Member 1">
         </div>
         <div class="col-12 col-lg-9 mb-5">
             <h1 style="color: #81CBC0;">Loh Min Er</h1>
@@ -22,17 +23,17 @@
             </p>
             <br>
             <div class="d-flex gap-4">
-                <button type="button" class="btn rounded-circle"
+                <button type="button" class="btn rounded-circle d-flex align-items-center justify-content-center"
                     style="width: 50px; height: 50px; background-color: #81CBC0; color: #0B131A">
-                    <i class="bi bi-linkedin"></i>
+                    <i class="bi bi-linkedin" style="font-size: 25px"></i>
                 </button>
-                <button type="button" class="btn rounded-circle"
+                <button type="button" class="btn rounded-circle d-flex align-items-center justify-content-cente"
                     style="width: 50px; height: 50px; background-color: #81CBC0; color: #0B131A">
-                    <i class="bi bi-envelope-at"></i>
+                    <i class="bi bi-envelope-at" style="font-size: 25px"></i>
                 </button>
-                <button type="button" class="btn rounded-circle"
+                <button type="button" class="btn rounded-circle d-flex align-items-center justify-content-cente"
                     style="width: 50px; height: 50px; background-color: #81CBC0; color: #0B131A">
-                    <i class="bi bi-github"></i>
+                    <i class="bi bi-github" style="font-size: 25px"></i>
                 </button>
             </div>
             {{-- <h4 style="color: #81CBC0;" class="mt-5">Skills</h4>
@@ -44,31 +45,139 @@
         </div>
     </div>
 
-    <div class="row align-items-center justify-content-center text-center">
+    <div class="row align-items-center justify-content-center text-center mb-5 pb-5">
         <h2 style="color: white;" class="mb-5">My Skills</h2>
-        <div class="rounded-3 p-4" style="background-color: white; max-width: 90vw">
-            <div class="d-flex flex-wrap justify-content-center gap-5">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/1/18/C_Programming_Language.svg"
-                    alt="C language" class="img-fluid" style="max-height: 100px;">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/c/c3/Python-logo-notext.svg" alt="C language"
-                    class="img-fluid" style="max-height: 100px;">
-                <img src="https://upload.wikimedia.org/wikipedia/de/e/e1/Java-Logo.svg" alt="C language"
-                    class="img-fluid" style="max-height: 100px;">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/d/d4/Javascript-shield.svg" alt="C language"
-                    class="img-fluid" style="max-height: 100px;">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/6/61/HTML5_logo_and_wordmark.svg"
-                    alt="C language" class="img-fluid" style="max-height: 100px;">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg"
-                    alt="C language" class="img-fluid" style="max-height: 100px;">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/b/b2/Bootstrap_logo.svg" alt="C language"
-                    class="img-fluid" style="max-height: 100px;">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/7/79/Flutter_logo.svg" alt="C language"
-                    class="img-fluid" style="max-height: 100px;">
-                    <img src="https://upload.wikimedia.org/wikipedia/de/d/dd/MySQL_logo.svg" alt="C language"
-                    class="img-fluid" style="max-height: 100px;">
-                    <img src="{{ asset('images/supabase-logo-icon.png') }}" alt="C language"
-                    class="img-fluid" style="max-height: 100px;">
+        {{-- <div class="rounded-3 p-4" style="background-color: white; max-width: 90vw"> --}}
+            <div class="d-flex flex-wrap justify-content-center gap-4 w-75">
+                <span
+                    class="badge rounded-pill d-inline-flex align-items-center justify-content-center gap-2 py-1 px-3 fs-5"
+                    style="background-color: #eef7ff; color: #004482;">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/1/18/C_Programming_Language.svg"
+                        alt="C language" style="height: 30px;">
+                    C language
+                </span>
+                <span
+                    class="badge rounded-pill d-inline-flex align-items-center justify-content-center gap-2 py-1 px-3 fs-5"
+                    style="background-color: #f4f7fb; color: #3773a5;">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/c/c3/Python-logo-notext.svg" alt="Python"
+                        style="height: 30px;">
+                    Python
+                </span>
+                <span
+                    class="badge rounded-pill d-inline-flex align-items-center justify-content-center gap-2 py-1 px-3 fs-5"
+                    style="background-color: #fdf9ed; color: #be6819;">
+                    <img src="https://upload.wikimedia.org/wikipedia/de/e/e1/Java-Logo.svg" alt="Java"
+                        style="height: 30px;">
+                    Java
+                </span>
+                <span
+                    class="badge rounded-pill d-inline-flex align-items-center justify-content-center gap-2 py-1 px-3 fs-5"
+                    style="background-color: #f9f7ed; color: #be9039;">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/d/d4/Javascript-shield.svg"
+                        alt="Javascript" style="height: 30px;">
+                    Javascript
+                </span>
+                <span
+                    class="badge rounded-pill d-inline-flex align-items-center justify-content-center gap-2 py-1 px-3 fs-5"
+                    style="background-color: #fdf4ef; color: #e44d26;">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/6/61/HTML5_logo_and_wordmark.svg"
+                        alt="HTML" style="height: 30px;">
+                    HTML
+                </span>
+                <span
+                    class="badge rounded-pill d-inline-flex align-items-center justify-content-center gap-2 py-1 px-3 fs-5"
+                    style="background-color: #f0f4fe; color: #264de4;">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg" alt="CSS"
+                        style="height: 30px;">
+                    CSS
+                </span>
+                <span
+                    class="badge rounded-pill d-inline-flex align-items-center justify-content-center gap-2 py-1 px-3 fs-5"
+                    style="background-color: #f4f1ff; color: #6c10f4;">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/b/b2/Bootstrap_logo.svg" alt="Bootstrap"
+                        style="height: 30px;">
+                    Bootstrap
+                </span>
+                <span
+                    class="badge rounded-pill d-inline-flex align-items-center justify-content-center gap-2 py-1 px-3 fs-5"
+                    style="background-color: #f0f8ff; color: #07599d;">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/7/79/Flutter_logo.svg" alt="Flutter"
+                        style="height: 30px;">
+                    Flutter
+                </span>
+                <span
+                    class="badge rounded-pill d-inline-flex align-items-center justify-content-center gap-2 py-1 px-3 fs-5"
+                    style="background-color: #e8fffe; color: #00758f;">
+                    <img src="https://upload.wikimedia.org/wikipedia/de/d/dd/MySQL_logo.svg" alt="MySQL"
+                        style="height: 30px;">
+                    MySQL
+                </span>
+                <span
+                    class="badge rounded-pill d-inline-flex align-items-center justify-content-center gap-2 py-1 px-3 fs-5"
+                    style="background-color: #effaf4; color: #1e7c53;">
+                    <img src="{{ asset('images/supabase-logo-icon.png') }}" alt="Supabase" style="height: 30px;">
+                    Supabase
+                </span>
+                <span
+                    class="badge rounded-pill d-inline-flex align-items-center justify-content-center gap-2 py-1 px-3 fs-5"
+                    style="background-color: #fef4ee; color: #f24e1e;">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/3/33/Figma-logo.svg" alt="Figma"
+                        style="height: 30px;">
+                    Figma
+                </span>
+                <span
+                    class="badge rounded-pill d-inline-flex align-items-center justify-content-center gap-2 py-1 px-3 fs-5"
+                    style="background-color: #f6f6f6; color: #6d6d6d;">
+                    <i class="bi bi-terminal-fill fs-2"></i>
+                    Network Configuration
+                </span>
+                <span
+                    class="badge rounded-pill d-inline-flex align-items-center justify-content-center gap-2 py-1 px-3 fs-5"
+                    style="background-color: #f6f6f6; color: #6d6d6d;">
+                    <i class="bi bi-search fs-2"></i>
+                    Network Troubleshooting
+                </span>
+                {{-- <img src="{{ asset('images/supabase-logo-icon.png') }}" alt="C language" class="img-fluid"
+                    style="max-height: 85px;"> --}}
             </div>
+            {{--
+        </div> --}}
+    </div>
+
+    <div class="row align-items-center justify-content-center text-center mb-5">
+        <h2 style="color: white;" class="mb-5">My Projects</h2>
+        <div id="carouselExample" class="carousel slide w-75">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <div class="d-flex justify-content-center">
+                        <div class="card w-75">
+                            <img src="{{ asset('images/test.jpg') }}" class="card-img-top" style="max-height: 400px"
+                                alt="">
+                            <div class="card-body">
+                                <h4 class="card-title">Card title</h4>
+                                <p class="card-text">Some quick example text to build on the card title and make up the
+                                    bulk
+                                    of the card’s content.</p>
+                                <a class="btn mt-3" role="button" href="#" style="background-color: #81CBC0; color: #0B131A; min-width: 150px;">Visit</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="{{ asset('images/supabase-logo-icon.png') }}" class="w-50" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="{{ asset('images/supabase-logo-icon.png') }}" class="w-50" alt="...">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
         </div>
     </div>
 </div>
