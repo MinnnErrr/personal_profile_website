461e98a82e417ce7783a9ab59333d90da12a1f03
winnie12 <xiaoying11121112@gmail.com>
2025-11-10 19:00:42 +0800

    xy_update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="position-relative text-center">
-        <img src="{{ asset('images/test.jpg') }}" class="img-fluid d-block w-100 pt-3" alt="bg_home" style="height: 650px; object-fit: cover;">
+        <img src="{{ asset('images/bg.png') }}" class="img-fluid d-block w-100 pt-3" alt="bg_home" style="height: 650px; object-fit: cover;">
         <h2 class="position-absolute top-50 start-50 translate-middle text-white fw-bold display-4">
             <span class="text-white">LARAVEL TEAM</span> 
             <span style="color: #81CBC0;">GROUP 8</span>
