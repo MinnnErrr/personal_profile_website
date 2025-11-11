c4f1e3c649be27904ed7b9523593681ddb5c63d6
MinnnErrr <lohminer22@gmail.com>
2025-11-11 12:15:45 +0800

    updated contact (add background image)

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -2,14 +2,21 @@
 
 @section('content')
 
-  <div class="row d-flex justify-content-center align-items-center" style="min-height: 80vh">
-    <div class="col-12 col-lg-6 text-center">
-      <h1>Contact Us</h1>
-      <p>Feel free to reach us by filling the form.</p>
+<div class="container-fluid d-flex justify-content-center align-items-center my-3 p-3 position-relative" style="min-height: 87vh; background: url('{{ asset('images/bg.png') }}') no-repeat center center; 
+            background-size: cover;
+            overflow: hidden;">
+
+  <div class="position-absolute top-0 start-0 w-100 h-100" 
+       style="background-color: rgba(0, 0, 0, 0.8); z-index: 1;"></div>
+
+  <div class="row w-100 align-items-center position-relative" style="z-index: 2;">
+    <div class="col-12 col-lg-6 text-center mb-5">
+      <h1 style="color: white">Contact Us</h1>
+      <p style="color: white">Feel free to reach us by filling the form.</p>
     </div>
 
     <div class="col-12 col-lg-6">
-      <form class="w-75 mx-auto p-4 border rounded-2">
+      <form class="w-75 mx-auto p-4 border rounded-2" style="background-color: white">
         <div class="mb-3">
           <label for="inputName" class="form-label">Name</label>
           <input type="text" class="form-control" id="inputName">
@@ -33,4 +40,5 @@
     </div>
   </div>
 
+</div>
 @endsection
