727a1e4d9a45b32763c2a2be61c136f9149c9a21
MinnnErrr <lohminer22@gmail.com>
2025-11-12 12:08:37 +0800

    deleted extra route

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
 Route::get('/', [App\Http\Controllers\PageController::class, 'showHome'])->name('home');
 
 Route::get('/CA22062', [App\Http\Controllers\PageController::class, 'showCA22062'])->name('CA22062');
